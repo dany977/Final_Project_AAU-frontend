4eb6a588701fd614329a9855acb33f2ed4fc825e
daniel <danielalemu33@gmail.com>
2025-11-23 09:49:09 +0300

    Updated API URL and build

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://final-project-aau-backend.onrender.com/api",
+  baseURL: "https://final-project-aau-backend.onrender.com",
  withCredentials: true,
 });
 
