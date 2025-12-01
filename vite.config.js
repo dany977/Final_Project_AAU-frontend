801194e450012f493935683f5ef92f3468a49831
daniel <danielalemu33@gmail.com>
2025-12-01 16:01:01 +0300

    Add vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-export default {
-  optimizeDeps: {
-    exclude: ['framer-motion'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/framer-motion/, /node_modules/],
-    },
-  },
-};
+// export default {
+//   optimizeDeps: {
+//     exclude: ['framer-motion'],
+//   },
+//   build: {
+//     commonjsOptions: {
+//       include: [/framer-motion/, /node_modules/],
+//     },
+//   },
+// };
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    historyApiFallback: true
+  }
+});
